63c9a97eb8c13753f7e8a3613b7b83927b068414
Matt Carlson <matthewthomascarlson@gmail.com>
2016-07-01 08:42:17 -0700

    get initial test to pass

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,1 +1,15 @@
 require('pry')
+
+class String
+  define_method(:word_count) do |to_find|
+    counter = 0
+    string_array = self.split(' ')
+    string_array.each do |item|
+# puts(item == to_find)
+      if item == to_find
+        counter += 1
+      end
+    end
+    counter
+  end
+end
