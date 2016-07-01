cd69b347f68466eb95e37e157e85e5fad1bf5a8d
Matt Carlson <matthewthomascarlson@gmail.com>
2016-07-01 08:45:56 -0700

    get two more tests passing in word_count_spec.rb

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -3,10 +3,10 @@ require('pry')
 class String
   define_method(:word_count) do |to_find|
     counter = 0
-    string_array = self.split(' ')
+    string_array = self.downcase().split(' ')
     string_array.each do |item|
 # puts(item == to_find)
-      if item == to_find
+      if item == to_find.downcase()
         counter += 1
       end
     end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,10 @@ describe('String#word_count') do
   it('returns how frequrnely a word appears in a given string') do
     expect(("If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?").word_count("peck")).to(eq(2))
   end
+  it('handles words with differing cases') do
+    expect(("If Peter Piper picked a peck of pickled peppers, where's the Peck of pickled peppers Peter Piper picked?").word_count("peck")).to(eq(2))
+  end
+  it('handles when a users search word is capitalized') do
+    expect(("If Peter Piper picked a peck of pickled peppers, where's the peck of pickled peppers Peter Piper picked?").word_count("Peck")).to(eq(2))
+  end
 end
