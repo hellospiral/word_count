de4d05d001a7ac2f4aae6b3335247f25a199a99a
Matt Carlson <matthewthomascarlson@gmail.com>
2016-07-01 09:23:03 -0700

    set up answer path in app.rb, add content to answer.erb, pass integration tests

--- app.rb
+++ app.rb
@@ -7,3 +7,10 @@ require('pry')
 get('/') do
   erb(:index)
 end
+
+get('/answer') do
+  @search_word = params.fetch("search_word")
+  @string_input = params.fetch("string_input")
+  @count = @string_input.word_count(@search_word).to_s()
+  erb(:answer)
+end
--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -9,3 +9,13 @@ describe('the root path', {:type => :feature}) do
     expect(page).to have_content("Input a sentence")
   end
 end
+
+describe('the answer path', {:type => :feature}) do
+  it('takes two strings and counts the appearances of one in the other') do
+    visit('/')
+    fill_in('string_input', :with => 'what is the  what')
+    fill_in('search_word', :with => 'what')
+    click_button('Count the words!')
+    expect(page).to have_content("2")
+  end
+end
