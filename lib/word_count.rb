cc2e3bc778802e0b302c94d5455a827dbd8f010e
Matt Carlson <matthewthomascarlson@gmail.com>
2016-07-01 09:02:42 -0700

    get all tests passing in word_count_spec.rb

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -5,7 +5,7 @@ class String
     counter = 0
     string_array = self.downcase.gsub(/[^a-z0-9\s]/i, '').split(' ')
     string_array.each do |item|
-      if item == to_find.downcase.strip()
+      if item == to_find.downcase.gsub(/[^a-z0-9\s]/i, '').strip()
         counter += 1
       end
     end
